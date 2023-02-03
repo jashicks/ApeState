cff9b828e08fb86f758b59c020d25cdb1230e849
Rodiru <jalynnsadie@hotmail.com>
2023-02-03 07:23:26 -0500

    + Added a new activity that is connected to the Main Activity
    + Added intent actions
    
    + Fixed crashing

--- app/src/main/java/com/example/apestate/MainActivity.kt
+++ app/src/main/java/com/example/apestate/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.apestate
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val secondActivityBtn: Button = findViewById (R.id.nextButton)
+        secondActivityBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/apestate/MainActivity2.kt
@@ -0,0 +1,31 @@
+package com.example.apestate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.apestate.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setSupportActionBar(binding.toolbar)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
